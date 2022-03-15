8cb6f34977f2dc47ff0cd3d8a3ffb15bb85635e6
Glen Meredith <mer16014@byui.edu>
2022-03-15 02:15:08 -0600

    finished w10 receipt

--- w10/receipt.py
+++ w10/receipt.py
@@ -1,13 +1,21 @@
 import csv
 from datetime import datetime
+import sys
 
 
 def main():
+
     products_dict = read_dict('w10/products.csv', 0)
-    print('ALL PRODUCTS')
-    print()
-    print(products_dict)
-    print()
+    # print('ALL PRODUCTS')
+    # print()
+    # print(products_dict)
+    # print()
+    print_receipt(products_dict)
+
+
+def calc_tip():
+    tip = float(input('How much do you want to tip? '))
+    return tip
 
 
 def read_dict(filename, key_column_index):
@@ -21,44 +29,75 @@ def read_dict(filename, key_column_index):
     Return: a compound dictionary that contains
         the contents of the CSV file.
     """
-    with open(filename, 'rt') as products_file:
-        reader = csv.reader(products_file)
-        next(reader)
-        products_dict = {}
-        for line in reader:
-            products_dict[line[key_column_index]] = [line[0], line[1], line[2]]
-        return products_dict
+    try:
+        with open(filename, 'rt') as products_file:
+            reader = csv.reader(products_file)
+            next(reader)
+            products_dict = {}
+            for line in reader:
+                products_dict[line[key_column_index]] = [
+                    line[0], line[1], line[2]
+                ]
+            return products_dict
+    except FileNotFoundError as file_not_found_err:
+        print(f'Error: {file_not_found_err}')
+        print(
+            'Looks like we\'re missing one of the files for your shopping experience. Please try again or emain mer16014@byui.edu :)'
+        )
+        sys.exit(1)
 
 
-def print_receipt():
-    print('Broulim\'s')
-    products_dict = read_dict('w10/products.csv', 0)
-    with open('w10/request.csv', 'rt') as request_file:
-        requests_dict = {}
-        reader = csv.reader(request_file)
-        print()
-        print('REQUESTED ITEMS')
-        print()
-        total_items = 0
-        subtotal = 0
-        for line in reader:
-            product_id = line[0]
-            if line[0] in products_dict:
+def print_receipt(products_dict):
+    try:
+        print('Broulim\'s')
+        with open('w10/request.csv', 'rt') as request_file:
+            reader = csv.reader(request_file)
+            next(reader)
+            print()
+            print('REQUESTED ITEMS')
+            print()
+            total_items = 0
+            subtotal = 0
+            for line in reader:
+                product_id = line[0]
+                # if line[0] in products_dict:
                 product_list = products_dict[product_id]
                 product_name = product_list[1]
                 quantity = line[1]
-                total_items += 1
+                total_items += int(quantity)
                 price = int(quantity) * float(product_list[2])
                 subtotal += price
-                print(f'{product_name} * {quantity} = ${price:.2f}')
-        sales_tax = 1.06
-        total = subtotal * 1.06
-        curr_date_time = datetime.now()
-        print(f'You bought {total_items} items today. ')
-        print(f'Subtotal: {subtotal}')
-        print(f'Total (after 6% tax): {total}')
-        print('Thanks for shopping with us today! Come back soon :) ')
-        print(f'- - --{curr_date_time:%A %I %M %p}-- - -')
+                print(f'{product_name}: {quantity} @ ${product_list[2]}')
+            sales_tax = subtotal * .06
+            tip = calc_tip()
+            total = subtotal + sales_tax + tip
+            curr_date_time = datetime.now()
+            print()
+            print(f'You bought {total_items} items today. ')
+            print(f'Subtotal: ${subtotal:.2f}')
+            print(f'Sales tax: ${sales_tax:.2f}')
+            print(f'Tip: {tip}')
+            print(f'Total: ${total:.2f}')
+            print('Thanks for shopping with us today! Come back soon :) ')
+            print(f'-- {curr_date_time:%A %b %d %I:%M %p %Y} --')
+
+    except FileNotFoundError as file_not_found_err:
+        print(f'Error: {file_not_found_err}')
+        print(
+            'Looks like we\'re missing one of the files for your shopping experience. Please try again or emain mer16014@byui.edu :)'
+        )
+
+    except PermissionError as permission_err:
+        print(f'Error: {permission_err}')
+        print(
+            'Looks like you don\'t have the right file permissions for your shopping experience. Please try again or email mer16014@byui.edu :)'
+        )
+
+    except KeyError as key_err:
+        print(f'Error: {key_err}')
+        print(
+            'Looks like we have a key error. Please try again or email mer16014@byui.edu :)'
+        )
 
 
 if __name__ == '__main__':
